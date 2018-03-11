f3105fad05a6a44ebf79d7448364cec99277519e
Kendokan <3757437+kendokan@users.noreply.github.com>
2018-03-11 13:32:14 -0700

    reformatted just about everything

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -1,105 +1,23 @@
 <?php
 session_start();
 
+// $debug = true;
 $baseUrl = '/auth';
-$selfUrl = $baseUrl. $_SERVER['PHP_SELF'];
+$selfUrl = $baseUrl . $_SERVER['PHP_SELF'];
 
-// We want to check auth status as quickly as possible, and before any headers are sent, so do this first
+
 if (isset($_REQUEST['status']))
-  if(getAuthStatus())
+  if (getAuthStatus())
     exit(http_response_code(200));
   else
     exit(http_response_code(401));
-elseif(isset($_REQUEST['login']))
-  login();
+elseif (isset($_REQUEST['login']))
+  login($_REQUEST['username'], $_REQUEST['password']);
 elseif(isset($_REQUEST['logout']))
   logout();
-else {
-  echo <<<EOT
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css" href="css/util.css">
-<link rel="stylesheet" type="text/css" href="css/main.css">
-<title>Authenticate</title>
-</head>
-<body onLoad="document.forms.login.username.focus()">
-EOT;
 
-$loginForm = <<<EOT
-<div class="limiter">
-<div class="container-login100">
-<div class="wrap-login100 p-t-90 p-b-30">
-<form class="login100-form validate-form" name="login" action="$selfUrl" method="post">
-<input type="hidden" name="login">
-<input type="hidden" name="req" value="{$_REQUEST['req']}">
-<div class="wrap-input100 validate-input m-b-16" data-validate="Please enter your username">
-<input class="input100" type="text" name="username" placeholder="Username">
-<span class="focus-input100"></span>
-</div>
-<div class="wrap-input100 validate-input m-b-20" data-validate = "Please enter password">
-<span class="btn-show-pass">
-<i class="fa fa fa-eye"></i>
-</span>
-<input class="input100" type="password" name="password" placeholder="Password">
-<span class="focus-input100"></span>
-</div>
-<div class="container-login100-form-btn">
-<button class="login100-form-btn">
-Login
-</button>
-</div>
-</form>
-</div>
-</div>
-</div>
-EOT;
 
-$logoutForm = <<<EOT
-<div class="limiter">
-<div class="container-login100">
-<div class="wrap-login100 p-t-90 p-b-30">
-<form class="login100-form validate-form" action="$selfUrl" method="post">
-<input type="hidden" name="logout">
-<div class="container-login100-form-btn">
-<button class="login100-form-btn">
-Logout
-</button>
-</div>
-</form>
-</div>
-</div>
-</div>
-EOT;
-
-  if (getAuthStatus())
-    echo $logoutForm;
-  else
-    echo $loginForm;
-
-  echo <<<"EOT"
-<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<script src="js/main.js"></script>
-</body>
-</html>
-EOT;
-}
-
-function login() {
-  if (!isset($_REQUEST['username']))
-    die('User name not provided.');
-  else {
-    $username = $_REQUEST['username'];
-  }
-
-  if (!isset($_REQUEST['password']))
-    die('Password not provided.');
-  else {
-    $password = $_REQUEST['password'];
-  }
-
+function login($username, $password) {
   if (password_verify($password, getPasswordHash($username))) {
     session_regenerate_id(true);
     $_SESSION['phpAuthRequest-Authenticated'] = true;
@@ -107,14 +25,10 @@ function login() {
 
     if (isset($_REQUEST['req']))
       header("Location: " . $_REQUEST['req']);
-    else
-      header("Location: $baseUrl");
-  } else {
-    logout();
   }
 }
 
-// Clears and kills the session
+
 function logout() {
   $_SESSION = array();
 
@@ -127,11 +41,9 @@ function logout() {
   }
 
   session_destroy();
-
-  header("Location: $baseUrl");
 }
 
-// Fetches password hash from sqlite
+
 function getPasswordHash($username) {
   $pdo = new \PDO('sqlite:../phpAuthRequest.sqlite3');
   $stmt = $pdo->prepare('SELECT password FROM users WHERE username=:username;');
@@ -140,7 +52,7 @@ function getPasswordHash($username) {
   return $stmt->fetchColumn();
 }
 
-// Checks if session is authenticated
+
 function getAuthStatus() {
   if (isset($_SESSION['phpAuthRequest-Authenticated'])) {
     if (($_SESSION['phpAuthRequest-Timestamp'] + 86400) > time()) {
@@ -150,3 +62,70 @@ function getAuthStatus() {
   }
 }
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="css/util.css">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+  <title>Authenticate</title>
+</head>
+
+<body onLoad="document.forms.login.username.focus()">
+
+  <div class="limiter">
+    <div class="container-login100">
+      <div class="wrap-login100 p-t-90 p-b-30">
+
+        <form class="login100-form validate-form" action="<?= $selfUrl ?>" method="post">
+<?php if (getAuthStatus()): ?>
+          <input type="hidden" name="logout">
+          <div class="container-login100-form-btn">
+            <button class="login100-form-btn">
+              Logout
+            </button>
+          </div>
+        </form>
+
+      </div>
+    </div>
+  </div>
+<?php else: ?>
+<?php if (isset($_REQUEST['req'])): ?>
+          <input type="hidden" name="req" value="<?= $_REQUEST['req'] ?>"
+<?php endif; ?>
+          <input type="hidden" name="login">
+          <div class="wrap-input100 validate-input m-b-16" data-validate="Please enter your username">
+            <input class="input100" type="text" name="username" placeholder="Username">
+            <span class="focus-input100"></span>
+          </div>
+          <div class="wrap-input100 validate-input m-b-20" data-validate = "Please enter password">
+            <span class="btn-show-pass">
+              <i class="fa fa fa-eye"></i>
+            </span>
+            <input class="input100" type="password" name="password" placeholder="Password">
+            <span class="focus-input100"></span>
+          </div>
+          <div class="container-login100-form-btn">
+            <button class="login100-form-btn">
+              Login
+            </button>
+          </div>
+        </form>
+
+      </div>
+    </div>
+  </div>
+<?php endif; ?>
+
+  <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+  <script src="js/main.js"></script>
+
+<?php if (isset($debug)): ?>
+<!-- $_SESSION
+<?php var_dump($_SESSION); ?>
+-->
+<?php endif; ?>
+</body>
+</html>
