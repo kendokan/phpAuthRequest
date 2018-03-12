d37279708b59d3eaadbd4120f6fd49904580878d
Kendokan <3757437+kendokan@users.noreply.github.com>
2018-03-11 18:45:21 -0700

    moved jquery

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -121,7 +121,7 @@ function getAuthStatus() {
   </div>
 <?php endif; ?>
 
-  <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+  <script src="js/jquery-3.2.1.min.js"></script>
   <script src="js/main.js"></script>
 
 <?php if (isset($debug)): ?>
