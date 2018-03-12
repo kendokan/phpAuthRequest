c7c079640e99e69314b03b3877ea8dfe5d55e65b
Kendokan <3757437+kendokan@users.noreply.github.com>
2018-03-12 10:18:53 -0700

    refactored with commenting

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -1,36 +1,81 @@
 <?php
-session_start();
+/**
+ * Todo:
+ *
+ * Error handling for database connection problems.
+ * Admin console.
+ * Allow user to change own password.
+ *
+ */
 
-// $debug = true;
-$baseUrl = '/auth';
+
+// Adds HTML comments to page source containing $_SESSION and $_REQUEST dumps.
+$debug = false;
+
+
+//Prepends path to PHP_SELF. Useful if calling this from NGINX proxy_pass.
+$baseUrl = '/login';
 $selfUrl = $baseUrl . $_SERVER['PHP_SELF'];
 
 
-if (isset($_REQUEST['status']))
-  if (getAuthStatus())
-    exit(http_response_code(200));
-  else
-    exit(http_response_code(401));
-elseif (isset($_REQUEST['login']))
-  login($_REQUEST['username'], $_REQUEST['password']);
-elseif(isset($_REQUEST['logout']))
-  logout();
+// Database connection, in PDO syntax.
+$database = array(
+  'dsn'       => 'sqlite:../phpAuthRequest.sqlite3',
+  'username'  => null,
+  'password'  => null
+);
 
 
-function login($username, $password) {
-  if (password_verify($password, getPasswordHash($username))) {
+// Sanitize redirect URL if it exists.
+$redirect = isset($_REQUEST['redirect']) ? filter_var($_REQUEST['redirect']) : null;
+
+
+//  Session must be started before continuing.
+session_start();
+
+
+// Main action call block.
+if (isset($_REQUEST['action'])) {
+  switch ($_REQUEST['action']) {
+    case 'status':
+      getAuthStatus() ? exit(http_response_code(200)) /* OK */ : exit(http_response_code(401)) /* Unauthorized */;
+      break;
+    case 'login':
+      login($database, $redirect, $_REQUEST['username'], $_REQUEST['password']);
+      break;
+    case 'logout':
+      logout();
+      break;
+  }
+}
+
+
+/**
+ * Validates login credentials, and optionally redirects browser upon success.
+ * @param  string $database database connection, in PDO syntax
+ * @param  string $redirect redirect browser to this URI after login success
+ * @param  string $username username
+ * @param  string $password password
+ * @return null
+ */
+function login($database, $redirect, $username, $password) {
+  if (password_verify($password, getPasswordHash($database, $username))) {
     $_SESSION['phpAuthRequest-Authenticated'] = true;
     $_SESSION['phpAuthRequest-Timestamp'] = time();
     session_regenerate_id(true);
 
-    if (isset($_REQUEST['req']))
-      header("Location: " . $_REQUEST['req']);
+    if (isset($redirect))
+      header("Location: " . $redirect);
   } else {
     logout();
   }
 }
 
 
+/**
+ * Removes cookies and destroys session.
+ * @return null
+ */
 function logout() {
   $_SESSION = array();
 
@@ -46,8 +91,14 @@ function logout() {
 }
 
 
-function getPasswordHash($username) {
-  $pdo = new \PDO('sqlite:../phpAuthRequest.sqlite3');
+/**
+ * Connects to database and gets user's password hash.
+ * @param  string $database database connection, in PDO syntax
+ * @param  string $username username
+ * @return string           password hash
+ */
+function getPasswordHash($database, $username) {
+  $pdo = new \PDO($database['dsn'], $database['username'], $database['password']);
   $stmt = $pdo->prepare('SELECT password FROM users WHERE username=:username;');
   $stmt->bindParam(':username', $username);
   $stmt->execute();
@@ -55,6 +106,12 @@ function getPasswordHash($username) {
 }
 
 
+/**
+ * Checks if an authenticated session exists. This function is called by
+ * NGINX's auth_request on every single page load, so it needs to be _fast_.
+ * Any delays in this function increase the user's page load time.
+ * @return bool true if authenticated
+ */
 function getAuthStatus() {
   if (isset($_SESSION['phpAuthRequest-Authenticated'])) {
     if (($_SESSION['phpAuthRequest-Timestamp'] + 86400) > time()) {
@@ -82,7 +139,7 @@ function getAuthStatus() {
 
         <form class="login100-form validate-form" action="<?= $selfUrl ?>" method="post">
 <?php if (getAuthStatus()): ?>
-          <input type="hidden" name="logout">
+          <input type="hidden" name="action" value="logout">
           <div class="container-login100-form-btn">
             <button class="login100-form-btn">
               Logout
@@ -94,10 +151,10 @@ function getAuthStatus() {
     </div>
   </div>
 <?php else: ?>
-<?php if (isset($_REQUEST['req'])): ?>
-          <input type="hidden" name="req" value="<?= $_REQUEST['req'] ?>">
+<?php if (isset($redirect)): ?>
+          <input type="hidden" name="redirect" value="<?= $redirect ?>">
 <?php endif; ?>
-          <input type="hidden" name="login">
+          <input type="hidden" name="action" value="login">
           <div class="wrap-input100 validate-input m-b-16" data-validate="Please enter your username">
             <input class="input100" type="text" name="username" placeholder="Username">
             <span class="focus-input100"></span>
